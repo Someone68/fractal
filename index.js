40ade42024b38ef19c1093c80d73c0661787a6bf
Someone68 <68.someone.m@gmail.com>
2023-09-21 18:08:18 -0700

    delete errors

--- index.js
+++ index.js
@@ -12,20 +12,3 @@ app.all("/", (req, res) => {
 app.listen(process.env.PORT || 3000, () => {
 	console.log(`Server listening on port ${process.env.PORT || 3000}`);
 });
-
-const config = {
-	authRequired: false,
-	auth0Logout: true,
-	secret: process.env["AUTH0_SECRET"],
-	baseURL: "https://fractal.cyclic.cloud",
-	clientID: "BpiMKH9fCvrTZVZ6DnPXetcK9Ypu55Ur",
-	issuerBaseURL: "https://dev-d6wbypv4bcbr56bq.us.auth0.com",
-};
-
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
-
-// req.isAuthenticated is provided from the auth router
-app.get("/", (req, res) => {
-	res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
-});
