444f3bc963790aa4ae27b320f2c9cfb548d79425
Someone68 <68.someone.m@gmail.com>
2023-09-21 17:23:11 -0700

    auth

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { auth } = require("express-openid-connect");
 
 app.use(express.static(path.join(__dirname, "./public")));
 
@@ -10,4 +11,21 @@ app.all("/", (req, res) => {
 
 app.listen(process.env.PORT || 3000, () => {
 	console.log(`Server listening on port ${process.env.PORT || 3000}`);
+});
+
+const config = {
+	authRequired: false,
+	auth0Logout: true,
+	secret: process.env["AUTH0_SECRET"],
+	baseURL: "https://fractal.cyclic.cloud",
+	clientID: "BpiMKH9fCvrTZVZ6DnPXetcK9Ypu55Ur",
+	issuerBaseURL: "https://dev-d6wbypv4bcbr56bq.us.auth0.com",
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get("/", (req, res) => {
+	res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
 });
